86ccecb225929101150f06e9a1ae87e87a812c9a
andre <anddreluis98@gmail.com>
2022-05-20 17:41:09 -0300

    EXEMPLAO

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ const express = require('express');
 const app = express();
 
 //localhost
-app.get("/", (request, response) => {
-    return response.send({message: 'hello world!'})
+app.get("/courses", (request, response) => {
+    return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+})
+
+app.post("/courses", (request, response) => {
+    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+})
+
+app.put("/courses/:id", (request, response) => {
+    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+})
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json(["Curso 6", "Curso 2", "Curso 4"]);
 })
 
 app.listen(3333);
